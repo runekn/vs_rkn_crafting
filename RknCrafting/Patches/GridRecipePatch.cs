f05111da649442f145daf55174d227583fe2f914
Rune Kofoed-Nissen <rune@beast.dk>
2026-07-07 16:53:41 +0200

    Remove inventory UI crafting grid

--- RknCrafting/Patches/GridRecipePatch.cs
+++ RknCrafting/Patches/GridRecipePatch.cs
@@ -2,7 +2,7 @@ using System.Collections.Generic;
 using HarmonyLib;
 using Vintagestory.API.Common;
 
-namespace RKN.Crafting;
+namespace RKN.Crafting.Patches;
 
 [HarmonyPatch(typeof(RecipeBase), "GenerateRecipesForTagOnlyIngredients")]
 public class GridRecipePatch
