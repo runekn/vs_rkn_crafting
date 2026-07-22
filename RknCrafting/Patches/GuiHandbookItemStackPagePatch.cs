34c2960c7d599abfde1852b76cced372420892e9
Rune Kofoed-Nissen <rune@beast.dk>
2026-07-22 16:33:27 +0200

    Redirect to output item when opening handbook for unfinished craft slot

---
+++ RknCrafting/Patches/GuiHandbookItemStackPagePatch.cs
@@ -0,0 +1,23 @@
+﻿using HarmonyLib;
+using RknCrafting.Entities;
+using Vintagestory.API.Common;
+using Vintagestory.GameContent;
+
+namespace RknCrafting.Patches;
+
+[HarmonyPatch(typeof(GuiHandbookItemStackPage), "PageCodeForStack")]
+public class GuiHandbookItemStackPagePatch
+{
+    static bool Prefix(ref ItemStack stack)
+    {
+        if (stack.Item is ItemUnfinishedCraft)
+        {
+            ItemStack outputStack = ItemUnfinishedCraft.GetOutputStack(stack);
+            if (outputStack != null)
+            {
+                stack = outputStack;
+            }
+        }
+        return true;
+    }
+}
